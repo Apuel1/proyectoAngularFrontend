f49811b3857343492be5fb9245df0eb39f92cfb5
Apuel1 <albardiazmartinezmanuel@gmail.com>
2022-12-28 05:08:39 -0300

    commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ContactameComponent } from './componentes/contactame/contactame.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    PortfolioComponent,
+    ProyectosComponent,
+    AcercaDeComponent,
+    ContactameComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/encabezado/encabezado.component.ts
@@ -0,0 +1,15 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
+@Component({
+  selector: 'app-encabezado',
+  templateUrl: './encabezado.component.html',
+  styleUrls: ['./encabezado.component.css']
+})
+export class EncabezadoComponent implements OnInit {
+  constructor(private portfolio:PortfolioService) {}
+
+  ngOnInit(): void {
+    this.portfolio.obtenerDatos();
+  }
+}
